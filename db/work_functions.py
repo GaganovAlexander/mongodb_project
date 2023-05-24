1c4196958c4eacdafda0ff6cbf0d9d23ef86bc65
Alexander <sashagaganov2000@gmail.com>
2023-05-24 23:15:08 +0300

    prettytable added

--- db/work_functions.py
+++ db/work_functions.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 from bson.objectid import ObjectId
+from prettytable import PrettyTable
 
 from db import db
 
@@ -15,6 +16,8 @@ def supplie(provider_id: str, product_id: str, amount: int):
     if db.products.update_one({"_id": product_id}, {"$inc": {"count": amount}}).raw_result.get('updatedExisting'):
         supplie_ = {"_id": str(ObjectId()), "provider_id": provider_id, "product_id": product_id, "amount": amount, "time": datetime.now()}
         db.supplies.insert_one(supplie_)
-        print(f"Successful suplie {supplie_}, new ammount = {prev_amount + amount}")
+        supplie_print = PrettyTable(supplie_)
+        supplie_print.add_row(supplie_.values())
+        print(f"Successful suplie \n{supplie_print}\nnew ammount = {prev_amount + amount}")
     else:
         print("Something went wrong")
